ead301878e52194c5167054c6dcba9117a8e77a4
Thiago Pereira <thiagosp1@hotmail.com>
2024-06-10 17:36:36 -0300

    removing old outputs with secrets information

--- outputs.tf
+++ outputs.tf
@@ -6,14 +6,6 @@ output "rds_cluster_arn" {
   value = aws_rds_cluster.primary.arn
 }
 
-output "secret_arn" {
-  value = aws_secretsmanager_secret.aurora_root_secret.arn
-}
-
-output "secret_name" {
-  value = aws_secretsmanager_secret.aurora_root_secret.name
-}
-
 output "rds_cluster_resource_id" {
   value = aws_rds_cluster.primary.cluster_resource_id
 }
