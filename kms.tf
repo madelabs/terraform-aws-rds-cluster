da3560b8c8f2d001aef4ccf5e2c2bbdc345ad306
Thiago Pereira <thiagosp1@hotmail.com>
2024-06-18 18:35:54 -0300

    adding a policy to allow managing the key

--- kms.tf
+++ kms.tf
@@ -1,4 +1,4 @@
-resource "aws_kms_key" "cluster_storage_key_1" {
+resource "aws_kms_key" "cluster_storage_key" {
   count                   = var.create_kms_key ? 1 : 0
   description             = "KMS key for encrypting storage in the cluster ${local.cluster_identifier}."
   deletion_window_in_days = var.kms_key_deletion_window_in_days
@@ -8,12 +8,12 @@ resource "aws_kms_key" "cluster_storage_key_1" {
 resource "aws_kms_alias" "alias" {
   count         = var.create_kms_key ? 1 : 0
   name          = "alias/${local.cluster_identifier}-storage-key"
-  target_key_id = aws_kms_key.cluster_storage_key_1[0].key_id
+  target_key_id = aws_kms_key.cluster_storage_key[0].key_id
 }
 
 resource "aws_kms_key_policy" "cluster_storage_key_policy" {
   count  = var.create_kms_key ? 1 : 0
-  key_id = aws_kms_key.cluster_storage_key_1[0].key_id
+  key_id = aws_kms_key.cluster_storage_key[0].key_id
   policy = data.aws_iam_policy_document.cluster_storage_key_policy[0].json
 }
 
@@ -65,51 +65,17 @@ data "aws_iam_policy_document" "cluster_storage_key_policy" {
 
     principals {
       type        = "AWS"
-      identifiers = ["arn:aws:iam::569510392077:role/tfc-deployment"]
+      identifiers = ["${data.aws_iam_session_context.context.issuer_arn}"]
     }
 
-    actions = [
-      "kms:Create*",
-      "kms:Describe*",
-      "kms:Enable*",
-      "kms:List*",
-      "kms:Put*",
-      "kms:Update*",
-      "kms:Revoke*",
-      "kms:Disable*",
-      "kms:Get*",
-      "kms:Delete*",
-      "kms:ScheduleKeyDeletion",
-      "kms:CancelKeyDeletion", ]
-
+    actions = ["kms:*"]
     resources = ["*"]
-
   }
 }
+
 
 data "aws_caller_identity" "current"{}
 
 data "aws_iam_session_context" "context" {
   arn = data.aws_caller_identity.current.arn
 }
-
-output "caller_arn"{
-  value = data.aws_caller_identity.current.arn
-}
-
-output "caller_user_id"{
-  value = data.aws_caller_identity.current.user_id
-}
-
-output "caller_id"{
-  value = data.aws_caller_identity.current.id
-}
-
-output "caller_account_id"{
-  value = data.aws_caller_identity.current.account_id
-}
-
-# locals{
-#   executing-identity = data.aws_caller_identity.current.id #"arn:aws:iam::569510392077:role/tfc-deployment"
-# }
-
