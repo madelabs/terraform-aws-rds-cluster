4126d56196ea4b62800781a164e12e1efc9dc109
Thiago Pereira <thiagosp1@hotmail.com>
2024-07-26 18:41:46 -0300

    putting read only cluster on the secret value

--- secrets.tf
+++ secrets.tf
@@ -6,11 +6,12 @@ resource "aws_secretsmanager_secret" "aurora_access_data" {
 resource "aws_secretsmanager_secret_version" "aurora_access_data_version" {
   secret_id = aws_secretsmanager_secret.aurora_access_data.id
   secret_string = jsonencode({
-    username = var.db_master_user,
-    password = local.db_password,
-    host     = aws_rds_cluster.primary.endpoint
-    port     = var.db_port,
-    dbname   = var.database_name
+    username      = var.db_master_user,
+    password      = local.db_password,
+    endpoint      = aws_rds_cluster.primary.endpoint,
+    read_endpoint = aws_rds_cluster.primary.reader_endpoint,
+    port          = var.db_port,
+    dbname        = var.database_name
   })
 
 }
