4beb9fc7a716941640e5e69ff4459f01164ea517
Thiago Pereira <thiagosp1@hotmail.com>
2024-07-29 19:46:10 -0300

    Adding default name for database

--- locals.tf
+++ locals.tf
@@ -4,4 +4,6 @@ locals {
   logs_set = compact([
     var.enable_postgresql_log ? "postgresql" : ""
   ])
+  db_password = var.generate_password == true ? random_password.aurora_password[0].result : data.aws_secretsmanager_secret_version.aurora_password[0].secret_string
+  db_name     = aws_rds_cluster.primary.database_name == null ? "postgres" : aws_rds_cluster.primary.database_name
 }
