a1842234ed01cb42a3610f6745afbd031913adb7
Thiago Pereira <thiagosp1@hotmail.com>
2024-08-01 14:35:26 -0300

    text description fix

--- variables.tf
+++ variables.tf
@@ -222,7 +222,7 @@ variable "cluster_tags" {
 }
 
 variable "instance_specific_tags" {
-  description = "An object specifying tags to be added to individual clusters. This works in conjunction with database_instance_count variable. Provided instance number cannot exceed instance count."
+  description = "Instance specifying tags to be added to individual instances. This works in conjunction with database_instance_count variable. Provided instance number cannot exceed instance count."
   type = list(object({
     instance_number = number
     tag_key         = string
