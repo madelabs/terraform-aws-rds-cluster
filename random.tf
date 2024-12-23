2a12ec24402ccdb07fbeb2e582e0293f31edf216
Thiago Pereira <thiagosp1@hotmail.com>
2024-12-23 15:51:23 -0300

    adding default region

--- random.tf
+++ random.tf
@@ -1,5 +1,5 @@
 resource "random_password" "aurora_password" {
   count   = var.generate_password == true ? 1 : 0
   length  = 10
-  special = false
+  special = true #this is just to avoid dropping the cluster in dev, to be removed. 
 }
